652e42e6135ae860d07f7434e04a7961af8e704a
aR0y2810 <127235610+aR0y2810@users.noreply.github.com>
2024-04-05 14:19:54 +0530

    Create a7q4.c
    3

---
+++ a7q4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+        if (argc!=3)
+        {
+                printf ("Incorrect number of arguments");
+                return 0;
+        }
+        FILE *fp1 = fopen(argv[1], "r");
+        FILE *fp2 = fopen(argv[2], "r");
+        if (fp1 == NULL || fp2 == NULL)
+        {
+                printf("No file content");
+                return 0;
+        }
+        char c1,c2;
+        while ((c1 = fgetc(fp1)) != EOF || (c2 = fgetc(fp2)) != EOF)
+        {
+                if (c1!=c2)
+                        return 0;
+        }
+        fclose(fp1);
+        fclose(fp2);
+        return 1;
+}
