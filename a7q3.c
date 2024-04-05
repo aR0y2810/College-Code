472d930d21f913e3034a27c47873548c132fd113
aR0y2810 <127235610+aR0y2810@users.noreply.github.com>
2024-04-05 14:05:22 +0530

    Update a7q3.c
    ee

--- a7q3.c
+++ a7q3.c
@@ -1,19 +1,24 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-int main() 
-{ 
-   FILE *fp1 = fopen("file1.txt", "r"); 
-   FILE *fp2 = fopen("file2.txt", "w"); 
-
-   if (fp1 == NULL) 
-   { 
-        printf("No file content"); 
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+        if (argc!=3)
+        {
+                printf ("Incorrect number of arguments");
+                return 0;
+        }
+        FILE *fp1 = fopen(argv[1], "r");
+        FILE *fp2 = fopen(argv[2], "w");
+        if (fp1 == NULL)
+        {
+                printf("No file content");
+                return 0;
+        }
+        int c;
+        while ((c = fgetc(fp1)) != EOF)
+                fputc(c, fp2);
+        fclose(fp1);
+        fclose(fp2);
         return 0;
-   } 
-   int c;
-   while ((c = fgetc(fp1)) != EOF) 
-        fputc(c, fp2); 
-   fclose(fp1); 
-   fclose(fp2);
-   return 0; 
-} 
+}
+~
