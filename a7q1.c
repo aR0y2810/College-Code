40feab6ed5f135a1b7f19d63d8b7ba1c20958244
aR0y2810 <127235610+aR0y2810@users.noreply.github.com>
2024-04-12 14:25:08 +0530

    Update a7q1.c
    added sub and mult

--- a7q1.c
+++ a7q1.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-typedef struct {
-    float real;       // Real part
-    float imaginary;  // Imaginary part
-} Complex;
-int main() {
-    Complex z1, z2;  
+struct complex
+{
+    float real;
+    float imaginary;
+};
+int main()
+{
+    struct complex z1, z2;
     printf("Enter the real part of z1: ");
     scanf("%f", &z1.real);
     printf("Enter the imaginary part of z1: ");
@@ -13,9 +14,15 @@ int main() {
     scanf("%f", &z2.real);
     printf("Enter the imaginary part of z2: ");
     scanf("%f", &z2.imaginary);
-    Complex sum;
+    struct complex sum, sub, mult;
     sum.real = z1.real + z2.real;
     sum.imaginary = z1.imaginary + z2.imaginary;
+    sub.real = z1.real - z2.real;
+    sub.imaginary = z1.imaginary - z2.imaginary;
+    mult.real = (z1.real * z2.real) - (z1.imaginary * z2.imaginary);
+    mult.imaginary = (z1.real * z2.imaginary) + (z1.imaginary * z2.real);
     printf("Sum of z1 and z2: %.2f + %.2fi\n", sum.real, sum.imaginary);
+    printf("Difference of z1 and z2: %.2f + %.2fi\n", sub.real, sub.imaginary);
+    printf("Multiply of z1 and z2: %.2f + %.2fi\n", mult.real, mult.imaginary);
     return 0;
 }
